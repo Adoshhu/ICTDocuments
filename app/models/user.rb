d8a53bfcde9d5f337f072c9cd290fb86e2f1d2a9
Adoshhu <adrianumalir@gmail.com>
2023-03-27 14:42:27 +0800

    Third commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+enum role: [:user, :moderator, :admin]
+  
+  after_initialize :set_default_role, :if => :new_record?
+  def set_default_role
+    self.role ||= :user
+  end
+  
+  def self.with_role(role)
+    where(role: roles[role])
+  end
+
+  def self.roles
+    { 'User' => 0, 'Moderator' => 1, 'Admin' => 2 }
+  end
+
+  def admin?
+    role == 'admin'
+  end
 end

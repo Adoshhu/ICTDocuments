889a9b3b480470a9c14adb29b56fa4aaf196f1b9
Adoshhu <adrianumalir@gmail.com>
2023-03-31 10:20:46 +0800

    route fixed

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -5,7 +5,7 @@ class Folder < ApplicationRecord
   belongs_to :parent, class_name: 'Folder', optional: true
   has_many :subfolders, class_name: "Folder", foreign_key: "parent_id", dependent: :destroy
   has_many_attached :files, dependent: :destroy
-
+  
 
 
   def self.roots

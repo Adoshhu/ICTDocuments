7a915991d7a630d43265c6087a6f7c48c741d95b
Adoshhu <adrianumalir@gmail.com>
2023-04-03 16:00:15 +0800

    Subfolder resoureces

--- app/controllers/subfolders_controller.rb
+++ app/controllers/subfolders_controller.rb
@@ -1,46 +1,59 @@
 class SubfoldersController < ApplicationController
-    before_action :set_user_folder, only: [:new, :create]
-  
-    # GET /subfolders/new
-    def new
-      @subfolder = Subfolder.new
-      @subfolder.parent_id = params[:parent_id] if params[:parent_id].present?
-    end
+  before_action :set_user_folder, only: [:show, :new, :create]
 
-    def index
-      @subfolders = Subfolder.all
-    end
+  def new
+    @subfolder = Subfolder.new
+    @subfolder.folder_id = params[:folder_id]
+    @subfolder.parent_id = params[:parent_id] if params[:parent_id].present?
+  end
 
-    def show
-      @user = User.find(params[:user_id])
-      @subfolders = @user.folders.find(params[:folder_id])
-      @current_folder = @subfolders
-      @sub = @current_folder.all
-    end
-
-    # POST /subfolders
-    def create
-      @subfolder = Subfolder.new(subfolder_params)
-      @subfolder.folder = @folder
-  
-      if @subfolder.save
-        redirect_to user_folder_path(@user, @folder), notice: 'Subfolder was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    # ...
-  
-    private
-  
-    def set_user_folder
-      @user = User.find(params[:user_id])
-      @folder = @user.folders.find(params[:folder_id])
-    end
-  
-    def subfolder_params
-      params.require(:subfolder).permit(:name, :parent_id,:folder_id)
-    end
+  def index
+    @user = current_user
+    @subfolders = Subfolder.joins(:folder).where(folders: { user_id: @user.id }, parent_id: nil)
   end
   
+  
+
+  def show
+    @subfolder = Subfolder.find(params[:id])
+  @current_folder = @subfolder.folder
+  end
+
+  def create
+    @subfolder = Subfolder.new(subfolder_params)
+    set_user_folder # move set_user_folder here
+    if @subfolder.parent_id.present?
+      parent = Subfolder.find_by(id: @subfolder.parent_id)
+  
+      if parent
+        @subfolder.parent_id = parent.id
+      else
+        render :new
+        return
+      end
+    end
+    @subfolder.folder = @folder # set folder after set_user_folder is called
+  
+    if @subfolder.save
+      redirect_to user_folder_subfolder_path(@user, @folder, @subfolder), notice: "Successfully created"
+    else
+      render :new
+    end    
+    
+    puts @subfolder.id.inspect
+    
+  end
+  
+
+  private
+
+  def set_user_folder
+    @user = current_user
+    @folder = @user.folders.find_by(id: params[:folder_id])
+    @subfolder = @folder
+  end
+
+  def subfolder_params
+    params.require(:subfolder).permit(:name, :parent_id, :folder_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :folders
+  resources :subfolders
   root 'page#index'
 
   resources :users do
